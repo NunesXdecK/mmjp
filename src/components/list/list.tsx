619cf8e984c1493ecc0f001e34d48d1b6709378c
José Neto <zswnunes@hotmail.com>
2022-05-02 12:29:40 -0400

    terceiro commit

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -1,31 +1,38 @@
-let tableContents = [
-]
-
-let lineTest = { codigo: "1PCN", codiNome: "Fazenda Arraial", cliente: "Conde Dokuu" }
+import { useEffect, useState } from "react"
+import data from "../../data/dados.json"
 
 const subtitle = "mt-1 max-w-2xl text-sm text-gray-500"
-const headerClassName = "px-4 py-5 text-md leading-6 font-medium text-gray-900"
-const rowClassName = "mt-1 text-sm text-gray-900 px-4 py-5"
-
+const titleClassName = "px-4 py-5 text-md leading-6 font-medium text-gray-900"
+const contentClassName = "mt-1 text-sm text-gray-900 px-4 py-5"
 
 export default function List() {
-    const rows = 20
+    const [inputSearch, setInputSearch] = useState("")
+    const [listItems, setListItems] = useState([])
 
-    for (let i = 0; tableContents.length < rows; i++) {
-        tableContents.push(lineTest)
-    }
+    useEffect(() => {
+        const lt = data.slice(0, 100)
 
-    const listItems = tableContents.map((tableContent, index) => (
-        <div key={index.toString() + tableContent.codigo}
-            className="bg-white p-4 rounded-sm shadow items-center">
-            <div className="flex">
-                <div><span className={headerClassName}>{index + 1}</span></div>
-                <div><span className={headerClassName}>{tableContent.codigo}</span></div>
-            </div>
-            <div><span className={rowClassName}>{tableContent.codiNome}</span></div>
-            <div><span className={rowClassName}>{tableContent.cliente}</span></div>
-        </div>
-    ))
+        const listItemsFiltered = lt.filter((element, index) => {
+            if (element.nome.toUpperCase().includes(inputSearch.toUpperCase())) {
+                console.log(inputSearch)
+                console.log(element.nome)
+            }
+            return element.nome.toUpperCase().includes(inputSearch.toUpperCase())
+        })
+
+        setListItems(
+            listItemsFiltered.map((element, index) => (
+                <div key={index.toString() + element.codigoBarras}
+                    className="bg-white p-4 rounded-sm shadow items-center">
+                    <div className="flex">
+                        <div><span className={titleClassName}>{index + 1}</span></div>
+                        <div><span className={titleClassName}>{element.nome}</span></div>
+                    </div>
+                    <div><span className={contentClassName}>{element.valorVenda}</span></div>
+                    <div><span className={contentClassName}>{element.dataU}</span></div>
+                </div>
+            )))
+    }, [])
 
     return (
         <div className="bg-white shadow overflow-hidden sm:rounded-lg">
@@ -35,24 +42,39 @@ export default function List() {
                     <h3 className="text-lg leading-6 font-medium text-gray-900">Lista de projetos</h3>
                     <p className={subtitle}>subtitulo lindo</p>
 
+
                     <div className="mt-5 flex">
-                        <div className="w-full">
+                        <div className="w-full self-end">
                             <label htmlFor="search-input" className={subtitle}>
                                 Pesquisa
                             </label>
                             <input
+                                onChange={(event) => {
+                                    setInputSearch(event.target.value)
+                                }}
                                 type="text"
                                 name="search"
                                 id="search-input"
-                                className="px-2 py-2 w-full sm:text-sm rounded-md "
-                            />
+                                className={`
+                                p-2 block w-full 
+                                shadow-sm rounded-md
+                                sm:text-sm 
+                                focus:ring-indigo-500 focus:border-indigo-500 
+                                `} />
                         </div>
-
-                        <div>
-                            <div className="px-4 py-3">
+                        <div className="pl-4 self-end">
+                            <div>
                                 <button
                                     type="submit"
-                                    className="justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                    className={`
+                                    justify-center py-2 px-4 
+                                    border border-transparent 
+                                    shadow-sm 
+                                    text-sm font-medium rounded-md text-white 
+                                    focus:outline-none focus:ring-2 focus:ring-offset-2 
+                                    focus:border-indigo-500 focus:ring-indigo-500
+                                    bg-indigo-600 hover:bg-indigo-700 
+                                    `}>
                                     Pesquisar
                                 </button>
                             </div>
