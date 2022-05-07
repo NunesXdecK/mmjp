7359004f28b62facb523f79b747f1130d0506eed
José Neto <zswnunes@hotmail.com>
2022-05-07 16:57:38 -0400

    070520221657

--- src/components/form/personForm.tsx
+++ src/components/form/personForm.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Button from "../button/button";
 import InputText from "../inputText/inputText";
 import PersonList from "../list/personList";
