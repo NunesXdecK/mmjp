5a3a11b9a1891bb066e5a90dcb5e301fdcd6b4b7
José Neto <zswnunes@hotmail.com>
2022-06-10 08:47:45 -0400

    terminando a area propriedade

--- src/db/firebaseDB.tsx
+++ src/db/firebaseDB.tsx
@@ -8,13 +8,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA4MNjIKQXTaZ-242BuAYd0PO58vY3C6H4",
-  authDomain: "mmjp-bfca6.firebaseapp.com",
-  projectId: "mmjp-bfca6",
-  storageBucket: "mmjp-bfca6.appspot.com",
-  messagingSenderId: "676571834239",
-  appId: "1:676571834239:web:359e7b18a3254b69099c4a",
-  measurementId: "G-J1CFCL6N6D"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
 // Initialize Firebase
